4b89d42928f4e820d50c47d43a2fae330c714ee1
KARIM.HARTI <hartikarim@gmail.com>
2026-06-03 14:51:49 +0100

    feat: initial commit with spring boot setup and ci-cd pipeline

---
+++ src/test/java/com/example/ecommercemicroservices/EcommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
